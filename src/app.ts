a2e3b6f802f5f65b9ef18402c7cd8197da5f18ff
noah-bar <noah.barberini@eduvaud.ch>
2024-01-10 10:23:48 +0100

    feat: install GoogleDataObject and GoogleLabelDetector libraries

--- src/app.ts
+++ src/app.ts
@@ -1,1 +1,5 @@
-console.log("hello world !")
+import { GoogleDataObject } from "bi_dataobject";
+import { GoogleLabelDetector } from "bi_label_detector";
+import { resolve } from "path"
+const keyFilename = resolve("./config/es-bin-noah.json")
+
