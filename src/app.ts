635aa52ac51e6e0444da60a7439f3c6b93bd1c5c
noah-bar <noah.barberini@eduvaud.ch>
2024-01-10 10:42:14 +0100

    feat: analyse image from bucket url

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,31 @@ import { GoogleDataObject } from "bi_dataobject";
 import { GoogleLabelDetector } from "bi_label_detector";
 import { resolve } from "path"
 import dotenv from 'dotenv'
+import { readFileSync } from "fs";
 
 dotenv.config()
 
 const keyFilename = resolve("./config/es-bi-noah.json")
 const bucketName = process.env.BUCKET_NAME as string
+const imagePath = resolve("./data/cat.jpg")
+const remoteFilename = "cat"
 
 const googleDataObject = new GoogleDataObject(keyFilename, bucketName)
 const googleLabelDetector = new GoogleLabelDetector(keyFilename)
+
+async function main() {
+    //upload cat image
+    const exists = await googleDataObject.doesExist(remoteFilename)
+    if(!exists) {
+        const buffer = readFileSync(imagePath)
+        await googleDataObject.upload(buffer, remoteFilename)
+    }
+
+    //analyse cat image from bucket url
+    const catUrl = await googleDataObject.publish(remoteFilename)
+    const labels = await googleLabelDetector.analyze(catUrl)
+
+    console.log(labels)
+}
+
+main()
